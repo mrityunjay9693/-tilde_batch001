a6ef4dec73ef0a4b3c1ce8b546773eb8ff2b77a1
mrityunjay9693 <56473482+mrityunjay9693@users.noreply.github.com>
2022-04-14 22:04:48 +0530

    data type range

---
+++ C/range.c
@@ -0,0 +1,12 @@
+//In c,   -2,147,483,648 -  2,147,483,647
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter value in a: ");
+    scanf("%d",&a);
+    printf("Value of a : %d ",a);
+
+    return 0;
+}
