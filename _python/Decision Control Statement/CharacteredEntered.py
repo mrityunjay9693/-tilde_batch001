9a968325537679074a8bb08da9f9277ddfeb496d
mrityunjay9693 <56473482+mrityunjay9693@users.noreply.github.com>
2022-05-04 20:52:31 +0530

    if and if-else

---
+++ _python/Decision Control Statement/CharacteredEntered.py
@@ -0,0 +1,18 @@
+# The program to determine the character entered by the user
+
+input = input("Press any Key : ")
+
+if(input.isalpha()):
+    print("The enetered input is consists of only alphabets.") # isalpha()
+if(input.isdigit()):
+    print("The enetered input/key consists of digit only.")
+if(input.isspace()):
+    print("The enetered key/input has white space character")
+
+
+
+
+#char = "Mrityunjay"
+#number = 12345
+#char.isalpha()
+#number.isdigit()
