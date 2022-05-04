38c55a89823626f1aaac405751544aa4a0e04516
mrityunjay9693 <56473482+mrityunjay9693@users.noreply.github.com>
2022-05-04 20:52:05 +0530

    if and if-else

---
+++ _python/Decision Control Statement/BiggerNumber.py
@@ -0,0 +1,11 @@
+# Program to print the bigger number between two number
+
+number1 = int(input("Enter first number : "))
+number2 = int(input("Enter second number : "))
+
+if(number1>number2):
+    print("Number1 is bigger")  # num1=3 , num2=5, 3>5
+else:
+    print("Number2 is bigger")
+
+
---
+++ _python/Decision Control Statement/voting.py
@@ -0,0 +1,37 @@
+#--------------
+#-if is a keyword and keyword is reserved word in programming language-------------
+#-we cannot named a variable or function by the name of keyword-------------
+#--------------
+age = int(input("Enter voter age : "))
+
+if( age >= 18):                     #------------------------
+    print("Eligible for Vote.")     #-------if block
+                                    #-----------------------
+#else:
+#   print("Not eligible for vote")       
+print("Not eligible for vote")                                  
+
+
+
+
+
+
+#--------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
