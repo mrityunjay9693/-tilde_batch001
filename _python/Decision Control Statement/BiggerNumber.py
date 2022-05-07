c4da3c7fc69e267172c581c317438643ad5ed356
mrityunjay9693 <56473482+mrityunjay9693@users.noreply.github.com>
2022-05-07 14:13:04 +0530

    if and if-else

--- _python/Decision Control Statement/BiggerNumber.py
+++ _python/Decision Control Statement/BiggerNumber.py
@@ -4,7 +4,7 @@ number1 = int(input("Enter first number : "))
 number2 = int(input("Enter second number : "))
 
 if(number1>number2):
-    print("Number1 is bigger")  # num1=3 , num2=5, 3>5
+    print("Number1 is bigger")  # number1 > number2   num1=3 , num2=1 : 3 > 1
 else:
     print("Number2 is bigger")
 
---
+++ _python/Decision Control Statement/EvenOdd.py
@@ -0,0 +1,9 @@
+#Program to print whether a number is even or odd
+odd = 13
+number = int(input("Enter the number:"))
+
+if(number % 2 == 0):  #number =15 -> number%2 : 15%2 : remaindere =1 : for even : remainder ==0 // for odd : remainder!=0
+    print("Number is even")
+else:
+    print("number is odd")
+
