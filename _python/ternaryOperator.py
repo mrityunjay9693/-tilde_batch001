296448c6a7c5f4e16f4568d233f211b46e14a951
mrityunjay9693 <56473482+mrityunjay9693@users.noreply.github.com>
2022-05-11 22:34:32 +0530

    20220511

---
+++ _python/ternaryOperator.py
@@ -0,0 +1,10 @@
+#Write a program to print the greater no among given two numbers.
+
+number1 = int(input("Enter the 1st number: "))
+number2 = int(input("Enter the 2nd number: "))
+
+Greater_number = number1 if number1 > number2 else number2
+print("Greater number : ",Greater_number)
+#print(Greater_number)
+
+
