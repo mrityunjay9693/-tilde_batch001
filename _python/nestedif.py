1755e168b639b8db94af22d6002f38cb1c2c3ed1
mrityunjay9693 <56473482+mrityunjay9693@users.noreply.github.com>
2023-08-06 22:25:54 +0530

    updates

---
+++ _python/nestedif.py
@@ -0,0 +1,17 @@
+#program to illustrates a nested-if
+
+number = int(input("Enter a number: ")) 
+
+if(number=0):
+    print("Number is zero. Provide a non-zero value")
+else:    
+    if(number):  #non zero returns true., Zero returns false
+    
+        if(number%2==0):  
+            print("Inner if works")
+        else:
+            print("Inner if condition becomes false. Number is not equal to 10")
+if(number>20):
+    print("Number is greater than 20")
+    
+        
