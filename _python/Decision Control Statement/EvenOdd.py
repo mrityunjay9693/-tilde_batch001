976b42f79804f06491507175d3297324689dc160
mrityunjay9693 <56473482+mrityunjay9693@users.noreply.github.com>
2023-08-06 22:39:01 +0530

    updates

--- _python/Decision Control Statement/EvenOdd.py
+++ _python/Decision Control Statement/EvenOdd.py
@@ -2,7 +2,7 @@
 odd = 13
 number = int(input("Enter the number:"))
 
-if(number % 2 == 0):  #number =15 -> number%2 : 15%2 : remaindere =1 : for even : remainder ==0 // for odd : remainder!=0
+if(number % 2 == 0):  #number =15 -> number%2 : 15%2 : remainder =1 : for even : remainder ==0 // for odd : remainder!=0
     print("Number is even")
 else:
     print("number is odd")
